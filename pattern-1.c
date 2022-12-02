a31bcc2f0fdca0d59a1bc7a0140209d712927be7
Moriom Akter Chowdhury <79042383+moriom16@users.noreply.github.com>
2022-12-02 21:44:51 +0600

    Add files via upload
    #include<iostream>
    using namespace std;
    int main(){
         int row,col,n;
            cin>>n;
            for(row=1; row<=n; row++){
                for(col=1;col<=n;col++){
                    cout<<"* ";
                }cout<<endl;
            }
    }

---
+++ pattern-1.c
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int row,col,n;
+        cin>>n;
+        for(row=1; row<=n; row++){
+            for(col=1;col<=n;col++){
+                cout<<"* ";
+            }cout<<endl;
+        }
+}
+
